b88c3e19ee9fb066e4e9826b7cfe7384b3d8a33d
Sergio <sergio.gonzalez@iesnervion.es>
2025-10-09 20:17:19 +0200

    3

--- Unidad 4/03Views/app/index.tsx
+++ Unidad 4/03Views/app/index.tsx
@@ -1,15 +1,84 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      
+      {/* Header */}
+      <View style={styles.header}>
+        <Text style={styles.text}>HEADER</Text>
+      </View>
+
+      {/* Body */}
+      <View style={styles.body}>
+        
+        {/* Columna izquierda */}
+        <View style={styles.sideColumn2}>
+          <Text style={styles.text}></Text>
+        </View>
+
+        {/* Contenido central */}
+        <View style={styles.content}>
+          <Text style={styles.text}>CONTENT</Text>
+        </View>
+
+        {/* Columna derecha */}
+        <View style={styles.sideColumn}>
+          <Text style={styles.text}></Text>
+        </View>
+
+      </View>
+
+      {/* Footer */}
+      <View style={styles.footer}>
+        <Text style={styles.text}>FOOTER</Text>
+      </View>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    height: 60,
+    backgroundColor: "#36e5f1ff", // color visible
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  body: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  sideColumn: {
+    width: 60,
+    backgroundColor: "#a34a4aff", // color visible
+    justifyContent: "center",
+    alignItems: "center",
+  },
+   sideColumn2: {
+    width: 60,
+    backgroundColor: "#a34a4aff", // color visible
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "#fff", // color visible del contenido
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  footer: {
+    height: 50,
+    backgroundColor: "#2196F3", // color visible
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#003366", // azul fuerte
+    fontWeight: "bold",
+  },
+});
