82e743e2ae8a93b116dcb0777f330a4a20ae7a8a
Sergio <sergio.gonzalez@iesnervion.es>
2025-11-25 19:09:02 +0100

    6 a medio terminar

---
+++ Unidad 6/Boletin01/EJ06/app/(drawer)/_layout.tsx
@@ -0,0 +1,26 @@
+import {Drawer} from 'expo-router/drawer';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function DrawerLayout() {
+  return (
+    <Drawer >
+      <Drawer.Screen name="index" 
+        options={{
+          
+          headerTitle: 'Home',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline"size={size} color={color} />
+          ),
+        }} />
+        <Drawer.Screen name="settings" 
+        options={{
+          
+          headerTitle: 'Settings',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline"size={size} color={color} />
+          ),
+        }} />
+    </Drawer>
+    );
+}
+
