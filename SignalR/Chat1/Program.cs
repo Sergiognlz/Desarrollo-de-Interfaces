808bc278e6c2d72737849ef1bc1ce29144398c3a
Sergiognlz <145675168+Sergiognlz@users.noreply.github.com>
2026-01-15 14:39:44 +0100

    1

--- SignalR/Chat1/Program.cs
+++ SignalR/Chat1/Program.cs
@@ -6,6 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddSignalR();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactNative",
+        policy =>
+        {
+            policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .SetIsOriginAllowed((host)=> true)
+                .AllowCredentials();
+        });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,9 +34,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+
+app.UseCors("AllowReactNative");
+
+
 app.UseAuthorization();
 
 app.MapRazorPages();
 app.MapHub<ChatHub>("/chatHub");
+
 
 app.Run();
--- SignalR/Chat1/obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
+++ SignalR/Chat1/obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cbfdbcfaaafe942c4eae7a59de4aea871d223b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+393a69482e38e0d93cc4e9574973dd7613671181")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
